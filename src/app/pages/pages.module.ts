92d9bfc318e98aa6033032cfa45ab06dd71a408b
Rene Mercado <rene.m@financieramas.app>
2018-10-01 14:31:14 -0500

    Input, output, viewchild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 import { PAGES_ROUTES } from './pages.routes';
+import { FormsModule } from '@angular/forms';
+
+// ng2 charts
+import { ChartsModule } from 'ng2-charts';
 
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 
+// temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 @NgModule({
 declarations: [
@@ -16,6 +23,8 @@ declarations: [
     DashboardComponent,
     ProgressComponent,
     Graficas1Component,
+    IncrementadorComponent,
+    GraficoDonaComponent
 ],
 exports: [ // Se agregan componentes que se ocupen utilizar desde otros modulos
     DashboardComponent,
@@ -24,7 +33,9 @@ exports: [ // Se agregan componentes que se ocupen utilizar desde otros modulos
 ],
 imports: [
     SharedModule,
-    PAGES_ROUTES
+    PAGES_ROUTES,
+    FormsModule,
+    ChartsModule
 ]
 })
 
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-progress',
-  templateUrl: './progress.component.html',
-  styles: []
+    selector: 'app-progress',
+    templateUrl: './progress.component.html',
+    styles: []
 })
 export class ProgressComponent implements OnInit {
 
-  constructor() { }
+    progreso1: number = 20;
+    progreso2: number = 30;
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+//     actualizar(event: number) {
+// this.progreso1 = event;
+//     }
 
 }
