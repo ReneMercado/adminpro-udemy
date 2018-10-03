55639ebe0c0cda59273a446f4f80f071a6c650c1
Rene Mercado <rene.m@financieramas.app>
2018-10-03 17:34:24 -0500

    Fin de la sección 7

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare function init_plugins(); // Se puede llamar asi cualquier script que este en un archivo fuera de angular
+// cosas que no funcionen en angular o typescript
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
+    init_plugins();
+  }
+
+  ingresar() {
+    this.router.navigate(['/dashboard']);
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; // NgFor nfif, pipes, necesarias para usarse en html
 
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,18 +11,19 @@ import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 @NgModule({
-declarations: [
-    NopagefoundComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent
-],
-exports: [ // Se agregan componentes que se ocupen utilizar desde otros modulos
-    NopagefoundComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent
-]
+    imports: [RouterModule, CommonModule],
+    declarations: [
+        NopagefoundComponent,
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent
+    ],
+    exports: [ // Se agregan componentes que se ocupen utilizar desde otros modulos
+        NopagefoundComponent,
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent
+    ]
 })
 
-export class SharedModule {}
+export class SharedModule { }
